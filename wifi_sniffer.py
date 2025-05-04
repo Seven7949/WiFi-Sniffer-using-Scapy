93e761ee62a7d80fac019698fe35b50623df7ce4
Seven7949 <scissorseven7949@gmail.com>
2025-05-04 10:35:31 +0530

    wifi_sniffer.py

---
+++ wifi_sniffer.py
@@ -0,0 +1,14 @@
+from scapy.all import *
+import os
+
+def packet_handler(packet):
+    if packet.haslayer(Dot11):
+        mac_src = packet[Dot11].addr2
+        mac_dst = packet[Dot11].addr1
+        ssid = packet[Dot11Elt].info.decode('utf-8', errors='ignore') if packet.haslayer(Dot11Elt) else ''
+        print(f"📶 Packet: {mac_src} ➡️ {mac_dst} | SSID: {ssid}")
+
+if __name__ == "__main__":
+    iface = input("Enter your WiFi interface in monitor mode (e.g., wlan0mon): ")
+    print("🔍 Sniffing WiFi packets... Press Ctrl+C to stop.\n")
+    sniff(iface=iface, prn=packet_handler, store=0)
